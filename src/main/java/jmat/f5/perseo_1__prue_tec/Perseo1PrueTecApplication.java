e7b2b29fe6e81f6e6e147d0bb61499f68c681bf8
jmat732 <jmatisamuel@gmail.com>
2024-09-10 20:24:34 +0200

    previo push

---
+++ src/main/java/jmat/f5/perseo_1__prue_tec/Perseo1PrueTecApplication.java
@@ -0,0 +1,13 @@
+package jmat.f5.perseo_1__prue_tec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Perseo1PrueTecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Perseo1PrueTecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jmat/f5/perseo_1__prue_tec/Perseo1PrueTecApplicationTests.java
@@ -0,0 +1,13 @@
+package jmat.f5.perseo_1__prue_tec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Perseo1PrueTecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
